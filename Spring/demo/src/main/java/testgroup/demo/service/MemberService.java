aa6e827d9cf50b50d4c7cc6d93b9c883cc3e0161
EunSeo <ibear3332@gmail.com>
2026-05-11 02:48:37 +0900

    Spring

---
+++ Spring/demo/src/main/java/testgroup/demo/service/MemberService.java
@@ -0,0 +1,46 @@
+package testgroup.demo.service;
+
+import testgroup.demo.domain.Member;
+import testgroup.demo.repository.MemberRepository;
+import testgroup.demo.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+    //회원 가입
+    public Long join(Member member){
+        //같은 이름의 중복 회원 불가
+        /*
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원명입니다.");
+        });
+        */
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원명입니다.");
+                });
+    }
+
+    //전체 회원 조회
+    public List<Member> findMembers(){
+        return MemberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
