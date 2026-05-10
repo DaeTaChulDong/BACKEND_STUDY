97b34c051fe229f4f1bd2435e1a43b0d8d5edcb6
EunSeo <ibear3332@gmail.com>
2026-05-10 17:08:55 +0900

    feat: Spring demo 깃허브 세팅

---
+++ Spring/demo/src/main/java/testgroup/demo/domain/Member.java
@@ -0,0 +1,19 @@
+package testgroup.demo.domain;
+
+public class Member {
+
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
